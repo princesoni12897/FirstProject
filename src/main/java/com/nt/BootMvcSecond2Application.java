d1d32fb8013031b2139c579f50e15c5bec33991e
prince <princesoni12897@gmail.com>
2022-03-10 16:47:32 +0530

    BootMVC_02

---
+++ src/main/java/com/nt/BootMvcSecond2Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcSecond2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcSecond2Application.class, args);
+	}
+
+}
